f079b4dd946dd356a04589a663027f0684c578cf
Ryan Lane <ryan@ryanalane.com>
2012-10-28 13:56:13 +0100

    Continued to flesh out the api_test.rb script

--- api_test.rb
+++ api_test.rb
@@ -1,5 +1,31 @@
 require 'tweetstream'
+require 'mongo'
 
-require 'tweestream_config'
+require_relative 'tweetstream_config'
 
-puts 'Hello'
+# Sets up connection to local Mongo database
+db = Mongo::Connection.new.db("ait_twitter_mining")
+
+tweets = db.collection("tweets")
+
+tweetstream_client = TweetStream::Client.new()
+
+# Handle delete and error messages 
+tweetstream_client.on_delete do |status_id, user_id|
+  puts "Removing #{status_id} from storage"
+  tweets.remove({"status" => status_id})
+end
+
+tweetstream_client.on_error do |message|
+  puts "Error received #{message}"
+end
+
+
+filter_params = Hash.new
+filter_params[:track] = Array['photography']
+
+tweetstream_client.filter(filter_params) do |status|
+  puts "#{status.text} - #{status.created_at}"
+  puts status
+#  tweets.insert(status)
+end
