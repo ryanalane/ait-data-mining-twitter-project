e5a2e4e1add6a75bc1bd4cfdbab52e87819139ea
Ryan Lane <ryan@ryanalane.com>
2012-10-30 17:13:58 +0100

    Added collection_script.rb, which will contain the keywork and mention parameters for mining the API
    Currently contains all of the hashtags which will be followed by the final script

---
+++ collection_script.rb
@@ -0,0 +1,68 @@
+require 'tweetstream'
+require 'mongo'
+
+require_relative 'tweetstream_config'
+
+# Sets up connection to local Mongo database
+db = Mongo::Connection.new.db("ait_twitter_mining")
+
+tweets = db.collection("tweets")
+
+tweetstream_client = TweetStream::Client.new()
+
+# Handle delete and error messages 
+tweetstream_client.on_delete do |status_id, user_id|
+  puts "Removing #{status_id} from storage"
+  tweets.remove({"status" => status_id})
+end
+
+tweetstream_client.on_error do |message|
+  puts "Error received #{message}"
+end
+
+
+filter_params = Hash.new
+filter_params[:track] = Array[
+  'election',
+  'Obama2012',
+  'Romney2012',
+  'MittRomney',
+  'Romney',
+  'Obama',
+  'BarackObama',
+  'POTUS',
+  'GOP',
+  'GOP2012',
+  'DNC',
+  'DNC2012',
+  'RNC',
+  'RNC2012',
+  'ObamaBiden',
+  'RomneyRyan',
+  'ObamaBiden2012',
+  'RomneyRyan2012',
+  'PaulRyan',
+  'JoeBiden',
+  'conservatives',
+  'liberals',
+  'tcot',
+  'tlot',
+  'WhiteHouse',
+]
+
+filter_params[:locations] = Array[]
+
+tweetstream_client.filter(filter_params) do |status|
+  puts status.created_at.to_s
+  puts status.place.full_name if status.place
+  tweet_data = {
+    'created_at' => status.created_at,
+    'text' => status.full_text,
+    'hashtags' => status.hashtags.collect{|x| x.text},
+    'mentions' => status.user_mentions.collect{|x| x.screen_name},
+    'place' => (status.place && status.place.full_name) || nil,
+    'coordinates' => (status.geo && status.geo.coordinates) || nil,
+    'user' => status.from_user
+  }
+  tweets.insert(tweet_data)
+end
