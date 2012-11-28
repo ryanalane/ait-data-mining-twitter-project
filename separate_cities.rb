19baef2ae369db07e6439724c0688d9f24928601
Ryan Lane <ryan@ryanalane.com>
2012-11-28 20:57:51 +0100

    Continued fleshing out the separate_cities.rb script

--- separate_cities.rb
+++ separate_cities.rb
@@ -5,16 +5,18 @@ db = Mongo::Connection.new.db("ait_twitter_mining")
 
 geotagged_tweets = db.collection("geotagged")
 
-DC_latitude_bounds = {'lower': 38.582526, 'higher': 39.253084}
-Houston_latitude_bounds = {'lower': 29.4862, 'higher': 30.4538}
+DC_latitude_bounds = {'lower' => 38.582526, 'higher' => 39.253084}
+Houston_latitude_bounds = {'lower' => 29.4862, 'higher' => 30.4538}
 
 geotagged_tweets.find.each do |tweet|
   latitude = tweet['coordinates'][0]
 
   if latitude >= DC_latitude_bounds['lower'] and latitude <= DC_latitude_bounds['higher']
-    puts 'DC!'
+    puts 'DC'
+  elsif latitude >= Houston_latitude_bounds['lower'] and latitude <= Houston_latitude_bounds['higher']
+    puts 'Houston'
   else
-    puts '?'
+    puts 'PROBLEM PROBLEM PROBLEM'
   end
 
 end
