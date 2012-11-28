da1bbd7a9c0a435dba180c6283ae57e7fb253e72
Ryan Lane <ryan@ryanalane.com>
2012-11-28 20:50:57 +0100

    Added a separate_cities.rb script that will eventually separate the mongo 'geotagged' collection into 'dc' and 'houston' collections

---
+++ separate_cities.rb
@@ -0,0 +1,20 @@
+require 'mongo'
+
+# Sets up connection to Mongo database
+db = Mongo::Connection.new.db("ait_twitter_mining")
+
+geotagged_tweets = db.collection("geotagged")
+
+DC_latitude_bounds = {'lower': 38.582526, 'higher': 39.253084}
+Houston_latitude_bounds = {'lower': 29.4862, 'higher': 30.4538}
+
+geotagged_tweets.find.each do |tweet|
+  latitude = tweet['coordinates'][0]
+
+  if latitude >= DC_latitude_bounds['lower'] and latitude <= DC_latitude_bounds['higher']
+    puts 'DC!'
+  else
+    puts '?'
+  end
+
+end
