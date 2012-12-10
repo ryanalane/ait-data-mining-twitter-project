01538cee1135fa514d794142cd8c46988b0a2ee0
Ryan Lane <ryan@ryanalane.com>
2012-12-10 16:40:59 +0100

    Added timezone_adjust.rb script to store tweets in their local timezone in a new mongo collection
    'dc_tweets' will be stored in 'dc_tweets_local', but with a -5 UTC adjustment

---
+++ timezone_adjust.rb
@@ -0,0 +1,18 @@
+require 'mongo'
+
+# Sets up connection to Mongo database
+db = Mongo::Connection.new.db("ait_twitter_mining")
+
+utc_collections = {'dc' => db.collection("dc_tweets"), 'houston' => db.collection("houston_tweets")}
+local_collections = {'dc' => db.collection("dc_tweets_local"), 'houston' => db.collection("houston_tweets_local")}
+utc_offsets = {'dc' => -5 * 3600, 'houston' => -6 * 3600}
+
+utc_collections.each_key do |city|
+
+  utc_collections[city].find.first(10).each do |tweet|
+    tweet['created_at'] = tweet['created_at'] + utc_offsets[city]
+    puts tweet['created_at']
+    local_collections[city].insert(tweet)
+  end
+
+end
